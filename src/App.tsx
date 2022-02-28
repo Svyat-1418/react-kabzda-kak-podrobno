42948a7d635543650cd179b3f44bb1683e716d0c
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-02-28 14:38:05 +0300

    02 - React кабзда как подробно (компоненты часть 2)

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log("App rendering")
     return (
         <div className="App">
-            This is App component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return <>This is App component</>
+}
+
 function Rating() {
+    console.log("Rating rendering")
     return (
         <div>
             <Star/>
@@ -23,23 +31,39 @@ function Rating() {
     )
 }
 
-/* Homework */
 function Star() {
+    console.log("Star rendering")
     return (
         <div>Star</div>
     )
 }
 
 function Accordion() {
+    console.log("Accordion rendering")
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
+    )
+}
+
+/* Homework */
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return (
+        <h3>Menu</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     )
 }
 
