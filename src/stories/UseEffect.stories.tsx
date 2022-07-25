684d38fdfc1ce378bfb348c54629c24813e7191c
Svyat-1418 <68824715+Svyat-1418@users.noreply.github.com>
2022-07-25 17:08:14 +0300

    22 - React кабзда как подробно: useEffect \ setTimeout \ setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -21,7 +21,7 @@ export const SimpleExample1 = () => {
     document.title = String(counter)
   }, [])
   useEffect(() => {
-    console.log("useEffect processing first render and when dependency(ies) changed")
+    console.log("useEffect processing when dependency(ies) chan")
     document.title = String(counter)
   }, [counter])
 
@@ -31,9 +31,33 @@ export const SimpleExample1 = () => {
         +
       </button>
 
-      {fake} fake
+      {fake}
     </div>
 
+    <div>
+      <button onClick={() => setCounter(state => state + 1)}>
+        +
+      </button>
+
+      {counter}
+    </div>
+  </>
+}
+
+
+export const SetTimeoutExample = () => {
+  console.log("SetTimeoutExample")
+
+  const [counter, setCounter] = useState(1)
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("setTimeout")
+      document.title = String(counter)
+    }, 1000)
+  }, [counter])
+
+  return <>
     <div>
       <button onClick={() => setCounter(state => state + 1)}>
         +
@@ -44,7 +68,23 @@ export const SimpleExample1 = () => {
   </>
 }
 
+export const SetIntervalExample = () => {
+  console.log("SetIntervalExample")
 
+  const [counter, setCounter] = useState(1)
 
+  useEffect(() => {
+
+    setInterval(() => {
+      console.log(`tick ${counter}`)
+      setCounter( state => state + 1)
+    }, 1000)
+
+  }, [])
+
+  return <div>
+      {counter} counter
+  </div>
+}
 
 
